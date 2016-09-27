4d2457bbadbde24aefe476dc1d57f72d0d295bbe
Sam <smr277@cornell.edu>
2016-09-27 11:48:46 -0400

    adding tester

---
+++ client_test.py
@@ -0,0 +1,14 @@
+import zmq
+
+#  Socket to talk to server
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+
+print("Waiting for messages from flightgear...")
+socket.connect("tcp://localhost:5556")
+
+socket.setsockopt_string(zmq.SUBSCRIBE, "")
+
+while(True):
+  string = socket.recv_string()
+  print(string)
