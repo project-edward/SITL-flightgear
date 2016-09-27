9a81fae561c23feb3c5dd26861d3c53bbac56757
Sam <smr277@cornell.edu>
2016-09-27 12:31:31 -0400

    working on protos

--- client_test.py
+++ client_test.py
@@ -7,7 +7,7 @@ socket = context.socket(zmq.SUB)
 print("Waiting for messages from flightgear...")
 socket.connect("tcp://localhost:5556")
 
-socket.setsockopt_string(zmq.SUBSCRIBE, "")
+socket.setsockopt_string(zmq.SUBSCRIBE, "GPS_MESSAGE")
 
 while(True):
   string = socket.recv_string()
--- xml_parser.py
+++ xml_parser.py
@@ -1,8 +1,9 @@
 import xml.etree.ElementTree as ET
 import asyncio
 import time
-import my_pb2
+import protos.core_pb2 as core_pb2
 import zmq
+from collections import OrderedDict
 
 class FgChunk:
   def __init__(self, node_name, name, unit, unit_format):
@@ -23,14 +24,14 @@ class FgChunk:
 
 class FgParseSend:
   def __init__(self):
-    self._fg_chunks = []
+    self._fg_chunks = OrderedDict([])
     context = zmq.Context()
     self._publisher = context.socket(zmq.PUB)
     self._publisher.bind("tcp://*:5556")
     self._previous_time = time.time()
 
   def AddChunk(self, chunk):
-    self._fg_chunks.append(chunk)
+    self._fg_chunks[chunk._name] = chunk
 
   def SetupFromXML(self, filename):
     tree = ET.parse(filename)
@@ -51,15 +52,23 @@ class FgParseSend:
     msg_list = message.split()
     if (len(self._fg_chunks) != len(msg_list)):
       print("Packet Error: Not the correct size")
-    for chunk_itm in zip(self._fg_chunks, msg_list):
+    for chunk_itm in zip(self._fg_chunks.values(), msg_list):
       chunk_itm[0].SetValue(chunk_itm[0].GetType()(chunk_itm[1]))
 
-    # TODO: Publish over protobuf
-    self._publisher.send_string("{}".format(self._fg_chunks[0].GetValue()))
-    person = my_pb2.Person()
-    person.name = "fred"
-    person.id = 0
-    person.email = "something@something.com"
+    imu = core_pb2.IMU()
+    imu.roll = self._fg_chunks["roll-deg"].GetValue()
+    imu.pitch = self._fg_chunks["pitch-deg"].GetValue()
+    imu.yaw = self._fg_chunks["heading-magnetic-deg"].GetValue()
+    imu_string = str(imu.SerializeToString())
+
+    gps = core_pb2.GPS()
+    gps.lat = self._fg_chunks["latitude-deg"].GetValue()
+    gps.lon = self._fg_chunks["longitude-deg"].GetValue()
+    gps.altitude = self._fg_chunks["altitude-ft"].GetValue()
+    gps_string = str(gps.SerializeToString())
+
+    self._publisher.send_string("IMU_MESSAGE:{}".format(imu_string))
+    self._publisher.send_string("GPS_MESSAGE:{}".format(imu_string))
 
 if __name__ == '__main__':
   fg_parser = FgParseSend()
